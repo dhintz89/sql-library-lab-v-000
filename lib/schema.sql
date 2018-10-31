49a3b8ca6ca95b3a6c196e5719aa81f5c650029e
Dan Hintz <dhintz@live.com>
2018-10-31 23:42:45 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,2 +1,32 @@
 CREATE TABLE Series (
-  id INTEGER PRIMARY KEY)
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author INTEGER,
+  sub_genre INTEGER);
+  
+CREATE TABLE Sub-Genre (
+  id INTEGER PRIMARY KEY,
+  name TEXT);
+  
+CREATE TABLE Authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT);
+  
+CREATE TABLE Books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year DATE,
+  series INTEGER);
+  
+CREATE TABLE Characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author INTEGER,
+  series INTEGER);
+  
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  book_id INTEGER,
+  character_id INTEGER);
