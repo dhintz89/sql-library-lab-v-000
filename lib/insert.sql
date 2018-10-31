c08e5d34d4fcc728f32e402bad9db16d74ce7d8b
Dan Hintz <dhintz@live.com>
2018-10-31 23:49:26 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,6 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES 
   ("Lord of the Rings", 1, 1), ("Harry Potter", 2, 2);
   
-INSERT INTO subgenres ()
+INSERT INTO subgenres (name) VALUES ("Adventure"), ("Magic");
+
+INSERT INTO authors ()
