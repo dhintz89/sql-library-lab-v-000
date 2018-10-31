edeb66c243b4ea5549c23db851cd57cadf4a2e55
Dan Hintz <dhintz@live.com>
2018-10-31 23:54:10 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,4 +7,4 @@ INSERT INTO authors (name) VALUES ("Tolkein"), ("Rowling");
 
 INSERT INTO books (title, year, series_id) VALUES
   ("Fellowship", 1990, 1), ("Two Towers", 1995, 1), ("Return", 2000, 1),
-  ("Sorceror's Stone", 2001, 2), ("Chamber of Secrets", )
+  ("Sorceror's Stone", 2001, 2), ("Chamber of Secrets", 2002, 2), ("Prisoner of Azkaban")
